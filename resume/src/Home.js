1f08685b7f00a3d4a9139b84df503390efdfe7ef
juccalle <juanccalle7@icloud.com>
2019-05-06 17:14:27 -0400

    added intro div file

---
+++ resume/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Intro from './components/Intro';
+import ReactDOM from 'react-dom';
+
+function Home(){
+
+    return(
+        <div id="root">
+            <p>Hello, world!</p>
+        </div>
+    ); 
+}
+
+export default Home;
---
+++ resume/src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Intro extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Hello, world!</p>
+            </div>
+        )
+    }
+}
+
+export default Intro;
